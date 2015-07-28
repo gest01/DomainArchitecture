47ef95a15be1566d02ea76b10a0b38c73739d8fd
stef <regieg@gmail.com>
2015-07-28 21:21:21 +0200

    object services added

---
+++ Example.CrossCutting/ObjectServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Example.CrossCutting.Logging;
+using Example.CrossCutting.Logging.Default;
+
+namespace Example.CrossCutting
+{
+    public static class ObjectServices
+    {
+        private static LoggerFactory _loggerFactory = new DefaultLoggerFactory();
+
+        public static LoggerFactory Logger { get { return _loggerFactory; } }
+
+        public static void SetLoggerFactory(LoggerFactory factory)
+        {
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            _loggerFactory = factory;
+        }
+    }
+}
