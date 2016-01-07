c51f2a454dceaf76316baf62362dd05148df2aa0
Thomas Bandixen <thomas@bandixen.ch>
2016-01-07 12:48:33 +0100

    Remove duplicates

--- Example.Api/Common/WebApi.cs
+++ Example.Api/Common/WebApi.cs
@@ -12,7 +12,6 @@ namespace Example.Api.Common
         public static void Configure()
         {
             GlobalConfiguration.Configure(ConfigureApi);
-            GlobalConfiguration.Configure(ConfigureApi);
         }
 
         private static void ConfigureApi(HttpConfiguration config)
