a2e18c5c95a5ee3deaaa0fef122f9b1355ee3399
gest01 <github@stefangeiger.ch>
2016-01-12 06:59:30 +0100

    update

---
+++ Example.Domain/Repositories/MyDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example.Domain.Repositories
+{
+    public interface IMyDataRepository
+    {
+        void GetMyData();
+    }
+
+
+    internal class MyDataRepository : IMyDataRepository
+    {
+        public void GetMyData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
