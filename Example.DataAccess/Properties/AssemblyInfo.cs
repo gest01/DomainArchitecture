7256b47286ae17c654dbfd3972e2572ace0b940b
gest01 <github@stefangeiger.ch>
2016-01-26 06:43:14 +0100

    cleanup

--- Example.DataAccess/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Example.DataAccess")]
-[assembly: AssemblyDescription("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("3eba8abe-0732-4c68-8b66-7a5e3e38357d")]
