daf773535952930b298f5729b7e71cf529a7c511
gest01 <github@stefangeiger.ch>
2016-02-03 20:49:00 +0100

    unit tests

---
+++ Example.Tests/Application/MyAppServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Example.Application;
+using Example.Tests.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example.Tests.Application
+{
+    [TestClass]
+    public class MyAppServiceTests : TestBase
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateItem_Null_Test()
+        {
+            // Arrange
+
+            MyDataRepositoryStub repository = new MyDataRepositoryStub();
+            IMyAppService appservice = new MyAppService(repository);
+
+            // Act
+
+            appservice.UpdateItem(null);
+
+            // Assert
+        }
+    }
+}
---
+++ Example.Tests/Domain/MyDataRepositoryStub.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Example.Domain.Entities;
+using Example.Domain.Repositories;
+
+namespace Example.Tests.Domain
+{
+    internal class MyDataRepositoryStub : IMyDataRepository
+    {
+        private List<MyEntity> _entities;
+
+        public MyDataRepositoryStub()
+        {
+            _entities = new List<MyEntity>();
+        }
+
+        public MyEntity Find(int id)
+        {
+            return _entities.SingleOrDefault(f => f.Id == id);
+        }
+
+        public IEnumerable<MyEntity> GetMyData()
+        {
+            return _entities;
+        }
+
+        public void UpdateEntity(MyEntity entity)
+        {
+
+        }
+    }
+}
