0a407fb4756e0da37caec05b74e512c5f8586040
gest01 <github@stefangeiger.ch>
2016-03-31 20:03:25 +0200

    unit test fixes

--- Example.Tests/Application/MyAppServiceTests.cs
+++ Example.Tests/Application/MyAppServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Example.Application;
+using Example.Application.DTO;
+using Example.Domain;
 using Example.Tests.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,6 +10,26 @@ namespace Example.Tests.Application
     [TestClass]
     public class MyAppServiceTests : TestBase
     {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UpdateItem_ItemNotFound_Test()
+        {
+            // Arrange
+
+            MyDataRepositoryStub repository = new MyDataRepositoryStub();
+            DataDomainService dds = new DataDomainService(repository);
+            IMyAppService appservice = new MyAppService(dds);
+
+            MyDemoDTO mydto = new MyDemoDTO() { Id = 123323 };
+
+            // Act
+
+            appservice.UpdateItem(mydto);
+
+            // Assert
+        }
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void UpdateItem_Null_Test()
@@ -15,7 +37,8 @@ namespace Example.Tests.Application
             // Arrange
 
             MyDataRepositoryStub repository = new MyDataRepositoryStub();
-            IMyAppService appservice = new MyAppService(repository);
+            DataDomainService dds = new DataDomainService(repository);
+            IMyAppService appservice = new MyAppService(dds);
 
             // Act
 
